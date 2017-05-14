e9fd09974181a0cfbc59551f111b5140e328b49c
theamitabh <amitprem@yahoo.com>
2017-05-14 16:21:09 +0400

    Add files via upload
    Added datepicker functionality

--- bagtag.js
+++ bagtag.js
@@ -1,28 +1,44 @@
 var app = angular.module('bagApp', []);
-app.controller('bagController' , function ($scope, $http) {
+app.directive('calendar', function () {
+            return {
+                require: 'ngModel',
+                link: function (scope, el, attr, ngModel) {
+                    $(el).datepicker({
+                        dateFormat: 'yy-mm-dd',
+                        onSelect: function (dateText) {
+                            scope.$apply(function () {
+                                ngModel.$setViewValue(dateText);
+                            });
+                        }
+                    });
+                }
+            };
+        })
+		
+app.controller('bagController', function ($scope, $http) {
 	$scope.showBagHeader = false;
-	$scope.pnrre = /\b\w{6}\b/;	
+	$scope.pnrre = /\b\w{6}\b/;
 
-	
+
 	$scope.selectBag = function (val) {
 		$scope.selectedBagTag = val.bagtag;
 		$scope.loadEvents();
 	}
-	
+
 	$scope.loadBagTags = function () {
 		$scope.listOfBags = null;
 		$scope.listOfBagEvents = null;
-		$scope.showBagHeader= false;
+		$scope.showBagHeader = false;
 		$scope.selectedBag = null;
-		
-		var apiurl = '/eyapi/baggage/v1/getguestbags?pnrnum=' + $scope.pnrNum.toUpperCase() + '&depdate=' + $scope.depDate ;
+
+		var apiurl = '/eyapi/baggage/v1/getguestbags?pnrnum=' + $scope.pnrNum.toUpperCase() + '&depdate=' + $scope.depDate;
 		$http.get(apiurl)
 
 			.success(function (data) {
 				$scope.listOfBags = data.bags;
 
 				if ($scope.listOfBags.length > 0) {
-					$scope.showBagHeader= true;
+					$scope.showBagHeader = true;
 					$scope.selectedBag = $scope.listOfBags[0].bagtag;
 					$scope.loadEvents();
 				}
@@ -30,8 +46,8 @@ app.controller('bagController' , function ($scope, $http) {
 			.error(function (data, status, headers, config) {
 				$scope.errorMessage = "Couldn't load the list of customers, error # " + status;
 			});
- 
-	
+
+
 
 	}
 
@@ -39,7 +55,7 @@ app.controller('bagController' , function ($scope, $http) {
 	$scope.loadEvents = function () {
 		$scope.listOfBagEvents = null;
 		//eyapi/baggage/v1/baghistory?bagtag={{x.bagtag}}&depdate={{trvlDate}}
-		var url = '/eyapi/baggage/v1/baghistory?bagtag=' + $scope.selectedBagTag + '&depdate=' + $scope.depDate ;
+		var url = '/eyapi/baggage/v1/baghistory?bagtag=' + $scope.selectedBagTag + '&depdate=' + $scope.depDate;
 		$http.get(url)
 			.success(function (data) {
 				$scope.listOfBagEvents = data.events;
@@ -48,5 +64,5 @@ app.controller('bagController' , function ($scope, $http) {
 				$scope.errorMessage = "Couldn't load the list of Orders, error # " + status;
 			});
 	}
-		
+
 });
